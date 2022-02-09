86e1be388de5dcd297c1b20b9e807425b2f9336b
faroooq <faroooq.in@gmail.com>
2022-02-09 10:41:43 +0530

    changes

--- src/app/shared/auth.guard.ts
+++ src/app/shared/auth.guard.ts
@@ -1,6 +1,4 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, CanDeactivate, Router, RouterStateSnapshot } from "@angular/router";
-import { ItemListComponent } from "../item-list/item-list.component";
 
 // auth.guard.ts
 @Injectable()
@@ -10,7 +8,7 @@ export class AuthGuard {
     // We will retrieve this info from some service later on.
     userLoggedIn: boolean = true;
 
-    constructor(private router: Router) { }
+    constructor() { }
 
     canActivate(): boolean {
         if (this.userLoggedIn) {
